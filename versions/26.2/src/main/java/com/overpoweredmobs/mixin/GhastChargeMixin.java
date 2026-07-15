681c6baad3c2de7c41e43116a39d05c137739ede
pastUnknownUser <evan6509@gmail.com>
2026-07-15 08:22:46 -0500

    fix MC 26.2: GiantAIMixin and GhastChargeMixin compat
    - GiantAIMixin: registerGoals removed from Giant in 26.2; inject
      into constructor (EntityType, Level) instead
    - GhastChargeMixin: Ghast inner class removed
      in 26.2 (Ghast AI reworked); no-op stub for 26.2

---
+++ versions/26.2/src/main/java/com/overpoweredmobs/mixin/GhastChargeMixin.java
@@ -0,0 +1,9 @@
+package com.overpoweredmobs.mixin;
+
+import net.minecraft.world.entity.monster.Ghast;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Ghast.class)
+public class GhastChargeMixin {
+}
---
+++ versions/26.2/src/main/java/com/overpoweredmobs/mixin/GiantAIMixin.java
@@ -0,0 +1,29 @@
+package com.overpoweredmobs.mixin;
+
+import com.overpoweredmobs.mixin.MobAccessor;
+
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.monster.Giant;
+import net.minecraft.world.entity.player.Player;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Giant.class)
+public class GiantAIMixin {
+
+    @Inject(method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;)V", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        Giant giant = (Giant) (Object) this;
+        var goals = ((MobAccessor) giant).getGoalSelector();
+        goals.addGoal(1, new MeleeAttackGoal(giant, 1.0, true));
+        goals.addGoal(2, new LookAtPlayerGoal(giant, Player.class, 16.0f));
+        goals.addGoal(3, new WaterAvoidingRandomStrollGoal(giant, 0.8));
+        goals.addGoal(4, new RandomLookAroundGoal(giant));
+    }
+}
